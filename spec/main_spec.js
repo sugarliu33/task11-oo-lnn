56417e51b8e4bfbd9e094472ffd2a6378950062a
sugarliu <450755513@qq.com>
2017-08-14 12:00:34 +0800

    init project

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -6,39 +6,12 @@ var sinonChai = require("sinon-chai");
 var expect = chai.expect;
 chai.use(sinonChai);
 
-var bottleSong = require("../lib/main.js");
+
 
 
 describe("buildBottlesSong", function(){
     sinon.spy(console, 'log');
 
-    it("return empty String given empty String", function(){
-
-        var result = bottleSong.buildBottleSong();
-        var expect_string = '';
-        
-        expect(expect_string).to.equal(result);
-    });
-
-    it("return one bottle String given one bottle", function(){
-
-        var result = bottleSong.buildBottleSong(1);
-        var expect_string = '1 bottle of beer on the wall, 1 bottle of beer.\n' +
-            'Take one down and pass it around, no more bottles of beer on the wall.';
-
-        expect(expect_string).to.equal(result);
-    });
-
-    it("return two bottles String given two bottles", function(){
-
-        var result = bottleSong.buildBottleSong(2);
-        var expect_string = '2 bottles of beer on the wall, 2 bottles of beer.' +'\n'+
-            'Take one down and pass it around, 1 bottle of beer on the wall.'+'\n'+
-            '1 bottle of beer on the wall, 1 bottle of beer.'+'\n'+
-            'Take one down and pass it around, no more bottles of beer on the wall.';
-
-        expect(expect_string).to.equal(result);
-    });
 
     xit("测试用例2", function(){
 
