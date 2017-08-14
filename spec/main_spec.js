36f60900c10624fae46acb3bd8ebd7b62f8c724d
sugarliu <450755513@qq.com>
2017-08-14 13:15:13 +0800

    add a test for checkcode function

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -7,11 +7,16 @@ var expect = chai.expect;
 chai.use(sinonChai);
 
 
+var main = require("../lib/main");
 
-
-describe("buildBottlesSong", function(){
+describe("calculateCheckcode", function(){
     sinon.spy(console, 'log');
-
+    it ("should return checkCode given postNumber", function () {
+        var arr = [9,5,7,1,3];
+        var result = main(arr);
+        var expectString = [9,5,7,1,3,5];
+        expect(result).to.Equal(expectString);
+    });
 
     xit("测试用例2", function(){
 
