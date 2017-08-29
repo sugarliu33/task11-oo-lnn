519fcb975677c401e5a7dc4df4068ec4cfbcb6d3
sugarliu <450755513@qq.com>
2017-08-29 15:10:23 +0800

    first submit

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -8,26 +8,24 @@ chai.use(sinonChai);
 
 
 var main = require("../lib/main.js");
+var studentInfo = require("./studentInfo");
+var classInfo = require("./classInfo");
 
-describe("calculateCheckcode", function(){
+function init() {
+    return query(`1. 添加学生
+2. 生成成绩单
+3. 退出
+请输入你的选择（1～3）：`);
+}
+
+describe("classInfo", function(){
     //sinon.spy(console, 'log');
-    it ("should return checkCode given postNumber without '-'", function () {
-        var arr = '95713';
-        var result = main.main(arr);
-        var expectStr = '957135';
-        expect(result).to.equal(expectStr);
-    });
-    it ("should return checkCode given postNumber with '-'", function () {
-        var arr = [5,5,5,5,5,'-',1,2,3,7];
-        var result = main.main(arr);
-        var expectStr = [ 5,5,5,5,5,1,2,3,7,2 ];
-        expect(result).to.equal(expectStr);
-    });
-
-    xit("测试用例2", function(){
-
-        var result = _.flatten(console.log.args).join("\n");
-        var expect_string = '';
-        expect(expect_string).to.equal(result);
+    it ("should return '学生的信息已添加", function () {
+        let stuIn = studentIn(`请输入学生信息（格式: 姓名,学号,班级,学科:成绩, ...），按回车提交：`);
+        // var str = "张三,001,2,数学:98,语文:99,英语:88,编程:90"
+        var classIn = new classInfo;
+        var ans = classIn.addStudent(stuIn);
+        var expectStr = '学生张三的成绩被添加';
+        expect(ans).to.equal(expectStr);
     });
 });
